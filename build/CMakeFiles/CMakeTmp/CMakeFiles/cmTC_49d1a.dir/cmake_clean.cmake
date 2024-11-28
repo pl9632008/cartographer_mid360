de4612f2e5823730d48a68e82d583c1308b0851e
pl9632008 <pl9632008@gmail.com>
2024-11-28 13:38:30 +0800

    fix : launch bug

---
+++ build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_49d1a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_49d1a.dir/testCXXCompiler.cxx.o"
+  "cmTC_49d1a"
+  "cmTC_49d1a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_49d1a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
