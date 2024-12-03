74cccfdfddcc67289bf0702100ba156e05d91ff2
pl9632008 <pl9632008@gmail.com>
2024-12-03 01:18:32 -0800

    feat : lua

--- src/cartographer_ros/cartographer_ros/configuration_files/mid360_localization.lua
+++ src/cartographer_ros/cartographer_ros/configuration_files/mid360_localization.lua
@@ -4,7 +4,7 @@ TRAJECTORY_BUILDER.pure_localization_trimmer = {
   max_submaps_to_keep = 3,
 }
 TRAJECTORY_BUILDER.pure_localization = true
-POSE_GRAPH.optimize_every_n_nodes = 1
+POSE_GRAPH.optimize_every_n_nodes = 0
 
 return options
 
