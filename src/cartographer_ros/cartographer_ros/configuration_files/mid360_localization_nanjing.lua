65fb8d7e5b0f05b46e8a336ff7c96e43b91dd5b7
pl9632008 <pl9632008@gmail.com>
2025-01-21 19:46:16 +0800

    feat: imu2baselink

--- src/cartographer_ros/cartographer_ros/configuration_files/mid360_localization_nanjing.lua
+++ src/cartographer_ros/cartographer_ros/configuration_files/mid360_localization_nanjing.lua
@@ -1,10 +1,10 @@
 include "mid360_mapping_nanjing.lua"
 
 TRAJECTORY_BUILDER.pure_localization_trimmer = {
-  max_submaps_to_keep = 3, --3
+  max_submaps_to_keep = 3, 
 }
 
-POSE_GRAPH.optimize_every_n_nodes = 20  --20
+POSE_GRAPH.optimize_every_n_nodes = 20  
 
 
 
